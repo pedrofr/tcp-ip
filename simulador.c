0898c7e0dcf2251c451c35c6ecfcc1fc5d3b0274
ENG10048 Protocolos de Comunicacao - A - Grupo 5 <eng10048_5@kalman.ece>
2019-06-13 15:13:16 -0300

    Adição de variáveis no simulador. Aula de 13/06/2019.

--- simulador.c
+++ simulador.c
@@ -13,62 +13,67 @@ void *simulate(void *args)
   char command[256]; 
   char argument[256];
 
+  double delta = 0;
+
+  double Max = 100;
+
+  double dt = 0.05;
+
   while(1)
     {
       while((*mod)==0);
       
       if (matches_numeric(pcomm->command, pcomm->argument, "OpenValve"))
 	{
-	  sprintf(argument, "%i!", (int)atof(pcomm->argument));
+	  double value = (int)atof(pcomm->argument);
+	    delta += value;
+	  sprintf(argument, "%i", value);
 	  strcpy(pcomm->command,"Open");
 	  strcpy(pcomm->argument,argument);
-          (*mod)=0;
 	}
       else if (matches_numeric(pcomm->command, pcomm->argument, "CloseValve"))
 	{
-	  sprintf(argument, "%i!", (int)atof(pcomm->argument));
+	  double value = (int)atof(pcomm->argument);
+	    delta -= value;
+	  sprintf(argument, "%i", value);
 	  strcpy(pcomm->command,"Close");
 	  strcpy(pcomm->argument,argument);
-	  (*mod)=0;
 	}
       else if (matches_numeric(pcomm->command, pcomm->argument, "SetMax"))
 	{
-	  sprintf(argument, "%i!", (int)atof(pcomm->argument));
+	  Max = (int)atof(pcomm->argument);
+	  sprintf(argument, "%i", value);
 	  strcpy(pcomm->command,"Max");
 	  strcpy(pcomm->argument,argument);
-	  (*mod)=0;
 	}
       else if (matches_no_arg(pcomm->command, pcomm->argument, "GetLevel"))
 	{
-	  sprintf(argument, "%i!", (int)atof(pcomm->argument));
+	  sprintf(argument, "%i!", (int)delta);
 	  strcpy(pcomm->command,"Level");
 	  strcpy(pcomm->argument,argument);
-	  (*mod)=0;
 	}
       else if (matches_no_arg(pcomm->command, pcomm->argument, "CommTest"))
 	{
 	  strcpy(pcomm->command,"Comm");
 	  strcpy(pcomm->argument,OK);
-	  (*mod)=0;
 	}
       else if (matches_no_arg(pcomm->command, pcomm->argument, "Start"))
 	{
 	  strcpy(pcomm->command,"Start");
 	  strcpy(pcomm->argument,OK);
-	  (*mod)=0;
 	}
       else if (matches_no_arg(pcomm->command, pcomm->argument, "Exit"))
 	{
 	  strcpy(pcomm->command,"Exit");
 	  strcpy(pcomm->argument,OK);
-	  (*mod)=0;
 	  break;
 	}else
 	{
 	  strcpy(pcomm->command, "NoMessageFound!");
 	  strcpy(pcomm->argument,"");
-	  (*mod)=0;
 	}
+
+          (*mod)=0;
     }
 
   printf("\nSaindo do simulador\n");
